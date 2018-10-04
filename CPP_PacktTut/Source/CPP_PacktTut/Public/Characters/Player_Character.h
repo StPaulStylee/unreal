c8e3e08bb8e92c2490404ea3f30cd1774a51d0b3
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2018-10-03 23:00:59 -0500

    forgot to save last change

--- CPP_PacktTut/Source/CPP_PacktTut/Public/Characters/Player_Character.h
+++ CPP_PacktTut/Source/CPP_PacktTut/Public/Characters/Player_Character.h
@@ -13,6 +13,11 @@ UCLASS()
 class CPP_PACKTTUT_API APlayer_Character : public ABase_Character
 {
 	GENERATED_BODY()
+
+public:
+	// You must always put the constructor in the public namespace so you can access it
+	// Constructor
+	APlayer_Character();
 	
 	
 	
