25e79c4023d537168c51aeffb3302594fb690966
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2018-10-04 21:30:56 -0500

    updating player character by implementing constructor data for health, ammo, and necessary launch functions at start of play

--- CPP_PacktTut/Source/CPP_PacktTut/Private/Characters/Player_Character.cpp
+++ CPP_PacktTut/Source/CPP_PacktTut/Private/Characters/Player_Character.cpp
@@ -1,10 +1,83 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Player_Character.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 APlayer_Character::APlayer_Character()
 {
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	if (SpringArm)
+	{
+		// Must allows SetupAttachment as all components are required to have a parent
+		SpringArm->SetupAttachment(GetCapsuleComponent());
+		SpringArm->RelativeLocation = FVector(-20.0f, 0.0f, 40.0f);
+		// How long the camera's spring arm is
+		SpringArm->TargetArmLength = 0.0f;
+		SpringArm->bDoCollisionTest = false;
+		SpringArm->bUsePawnControlRotation = true;
+		// Setting what we can and cannot inherit from the pawn rotation
+		SpringArm->bInheritPitch = false;
+		SpringArm->bInheritRoll = true;
+		SpringArm->bInheritYaw = true;
+	}
 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	if (Camera)
+	{
+		Camera->FieldOfView = 90.0f;
+		Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	}
+
+	FP_Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_MESH"));
+	if (FP_Mesh)
+	{
+		FP_Mesh->SetupAttachment(Camera);
+	}
+
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMesh"));
+	if (GunMesh)
+	{
+		GunMesh->SetupAttachment(Camera);
+	}
+
+	MaxHealth = 100.0f;
+	CurrentHealth = MaxHealth;
+
+	bCanShoot = true;
+	bUnlimitedAmmo = false;
+	MaxAmmo = 10;
+	CurrentAmmo = MaxAmmo;
+}
+
+void APlayer_Character::BeginPlay()
+{
+	// We call Super::BeginPlay to call it's parents BeginPlay.. Since it is a virtual method
+	// we can trust that it's parent has it and is doing the same thing. If all SubClasses are calling
+	// super on their virtual methods, we can trust that they get called all the way up 
+	// the chain and this is good practice
+	Super::BeginPlay();
+
+	// For when the max ammo value was changed
+	if (CurrentAmmo != MaxAmmo)
+	{
+		CurrentAmmo = MaxAmmo;
+	}
+}
+
+void APlayer_Character::SetupPlayerInputComponent(UInputComponent * PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Assertion check - This is a macro that will cause the program to crash
+	// if it is false. Think of it has a super aggressive if statment
+	check(PlayerInputComponent);
+}
+
+void APlayer_Character::OnDeath_Implementation()
+{
 }
 
 
--- CPP_PacktTut/Source/CPP_PacktTut/Public/Characters/Player_Character.h
+++ CPP_PacktTut/Source/CPP_PacktTut/Public/Characters/Player_Character.h
@@ -14,12 +14,52 @@ class CPP_PACKTTUT_API APlayer_Character : public ABase_Character
 {
 	GENERATED_BODY()
 
+	// the meta tag allow for extra features in view property... Since this is a private property, view propety allows us to edit in Blueprint
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* SpringArm;
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent * Camera;
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USkeletalMeshComponent * FP_Mesh;
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USkeletalMeshComponent* GunMesh;
+
 public:
 	// You must always put the constructor in the public namespace so you can access it
 	// Constructor
 	APlayer_Character();
+
+	UFUNCTION(BlueprintPure, Category = "Character|Gun|Ammo")
+		int32 GetCurrentAmmo() const { return CurrentAmmo; }
+
+	UFUNCTION(BlueprintPure, Category = "Character|Gun|Ammo")
+		int32 GetMaxAmmo() const { return MaxAmmo; }
+
+	UFUNCTION(BlueprintCallable, Category = "Character|Gun|Ammo")
+		void SetCurrentAmmo(int NewAmmo) { CurrentAmmo = NewAmmo; }
+
+protected:
 	
+	// We inherit the BeginPlay and OnDeath_Implementation from our Base class
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	virtual void OnDeath_Implementation() override;
+
+	/** Check and see if we have any remaining ammo */
+	bool HasAmmo() const { return (CurrentAmmo > 0); }
+
+	/** Flag for when the play can shoot */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character|Gun")
+		bool bCanShoot;
 	
-	
+	/** Flag for when the play can shoot unlimited amount of bullets */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character|Gun|Ammo")
+		bool bUnlimitedAmmo;
+
+	int CurrentAmmo;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Character|Gun|Ammo")
+		int MaxAmmo;
 	
 };
